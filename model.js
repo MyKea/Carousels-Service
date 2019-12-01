917d32c3af1a4242e105c8468470738876e56c8b
Gabe-Lee <gla.ander@gmail.com>
2019-12-01 16:34:55 -0600

    Define database schema

---
+++ model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  _id: { type: Number, required: true },
+  name: String,
+  description: String,
+  price: Number,
+  rating: Number,
+  reviews: Number,
+  options: Boolean,
+  image: Buffer,
+  related: {
+    loose: [Number],
+    close: [Number],
+  },
+});
+
+module.exports = mongoose.model('Product', productSchema);
